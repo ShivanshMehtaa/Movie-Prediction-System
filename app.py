043efcb77b8b7e0e2fec032ed9ee8923c238f69a
Shivansh mehta <112550037+ShivanshMehtaa@users.noreply.github.com>
2023-08-24 18:53:42 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+def recommend(movie):
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])),reverse=True,key = lambda x: x[1])
+    recommended_movies=[]
+    for i in distances[1:6]:
+        recommended_movies.append(movies.iloc[i[0]].title)
+
+    return recommended_movies
+
+movies_dict = pickle.load(open('movie_dict.pkl','rb'))
+movies  =pd.DataFrame(movies_dict)
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+st.title('Movie Recommendation System')
+
+selected_movie_name = st.selectbox('Choose Movies from the given DropDown List',movies['title'].values)
+
+if st.button('Recommend'):
+    recommendations = recommend(selected_movie_name)
+    for i in recommendations:
+        st.write(i)
+
